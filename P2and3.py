0e11d9cf344d8af0d4ba3d395bc6261ea9f3e8c9
conniehtli <connie1488>
2018-03-27 13:19:54 -0400

    Initial commit

---
+++ P2and3.py
@@ -0,0 +1,6 @@
+from scipy.stats import binom
+
+print('The number of participants that survived beyond 5 years in a cohort of N particpants follow a binominal '
+      'distribution. ')
+
+n, p = 5, 0.4

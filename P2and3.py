edc13f8ad4040643dd6134ab222c15578716e044
conniehtli <connie1488>
2018-03-30 17:48:06 -0400

    Update

--- P2and3.py
+++ P2and3.py
@@ -1,6 +1,11 @@
 from scipy.stats import binom
 
-print('The number of participants that survived beyond 5 years in a cohort of N particpants follow a binominal '
-      'distribution. ')
+print('--------------------Problem 2-----------------------')
+print('The number of participants that survived beyond 5 years in a cohort of N particpants follows a binominal '
+      'distribution with the parameters '
+      'N (number of individuals in the cohort) and q (probabilty of 5-year survival caluclated in problem 1). ')
 
-n, p = 5, 0.4
+print('--------------------Problem 3-----------------------')
+
+print('The likelihood that 400 of 573 participants have survived at the end of a 5 year study period, given 50% surivival probability:')
+print(binom.pmf(k=400, n=573, p=0.5))
